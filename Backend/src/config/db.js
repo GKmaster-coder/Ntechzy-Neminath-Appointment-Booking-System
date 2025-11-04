3f10ead83ec5afe473811a853e63b03698014ef9
Gkmaster-coder <gouravkashyap2468@gmail.com>
2025-11-04 12:54:26 +0530

    refactor: simplify MongoDB connection options in db.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,17 +4,15 @@ import { DB_NAME } from "../constant.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
 
-    console.log(`✅ MongoDB Connected! DB Host: ${connectionInstance.connection.host}`);
+    console.log(
+      `✅ MongoDB Connected! DB Host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1); // Stop the app if DB is not connected
+    process.exit(1);
   }
 };
 
